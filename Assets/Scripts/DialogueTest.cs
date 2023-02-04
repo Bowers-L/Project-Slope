4798e6345f5f9569c16c55d048d2bbbe9473d455
bchen27665 <bchen27665@gmail.com>
2023-02-04 00:21:30 -0500

    - Dialogue can branch based off of set var

--- Assets/Scripts/DialogueTest.cs
+++ Assets/Scripts/DialogueTest.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityTimer;
+using Yarn.Unity;
 
 public class DialogueTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Timer dialogueProgressTimer;
+    [SerializeField]
+    GameObject DialogueRunnerObject;
+    [SerializeField]
+    GameObject DialogueView;
+    DialogueRunner _dialogueRunner;
+    InMemoryVariableStorage _inMemoryVariableStorage;
+    DialogueAdvanceInput _dialogueInput;
+    LineView _lineView;
+
+    Timer dialogueProgressCooldownTimer;
+    bool dialogueProgressCooldown = true;
+
     void Start()
     {
-        
+        _dialogueRunner = DialogueRunnerObject.GetComponent<DialogueRunner>();
+        _inMemoryVariableStorage = DialogueRunnerObject.GetComponent<InMemoryVariableStorage>();
+        _dialogueInput = DialogueRunnerObject.GetComponent<DialogueAdvanceInput>();
+        _lineView = DialogueView.GetComponent<LineView>();
+
+        _inMemoryVariableStorage.SetValue("$jamming", true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ProgressDialogue();
+            dialogueProgressCooldown = false;
+            dialogueProgressCooldownTimer = Timer.Register(1f, () => dialogueProgressCooldown = true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _inMemoryVariableStorage.SetValue("$jamming", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _inMemoryVariableStorage.SetValue("$jamming", false);
+        }
+    }
+
+    void ProgressDialogue()
+    {
+        if (dialogueProgressCooldown) _lineView.UserRequestedViewAdvancement();
     }
 }
