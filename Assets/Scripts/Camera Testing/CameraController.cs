176883aec5c9f48ffb877bb63a320379838e936a
Lominoe <v.x.nguyen231@gmail.com>
2023-02-05 00:15:28 -0500

    Characters in Scene

---
+++ Assets/Scripts/Camera Testing/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
